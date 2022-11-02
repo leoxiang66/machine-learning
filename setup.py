fda421d3a5f2dde6535fba2aa315e7ac5905f75c
Tao Xiang <xiang.tao@outlook.de>
2022-11-02 17:30:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ requirements = [
 
 setup(
     name="ml_leoxiang66",
-    version="0.1.0",
+    version="0.1.1",
     author="Tao Xiang",
     author_email="tao.xiang@tum.de",
     description="A package of RL algorithms",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/leoxiang66/machine-learning",
+    url="https://github.com/leoxiang66/machine-learning/tree/MLO",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[

2e9fc413e98511dbb6bd23a7a45ceb42f74fb3a7
Tao Xiang <xiang.tao@outlook.de>
2022-11-08 20:46:14 +0100

    1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setup(
     name="ml_leoxiang66",
-    version="0.2.0",
+    version="0.3.0",
     author="Tao Xiang",
     author_email="tao.xiang@tum.de",
     description="A package of RL algorithms",
